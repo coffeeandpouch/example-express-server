f8a4356d08950ab63662e7f32414358ed3bcfe2c
Sam Einsidler <sameinsidler@me.com>
2020-11-14 13:08:49 -0600

    Added Gretting

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const PORT = process.env.PORT || 8000;
 const app = express();
 
 app.get("/", (req, res) => res.send("Hello"));
+app.get("/greeting", (req, res) => res.send(`Hello ${req.query.name}`));
 
 app.listen(PORT, () => {
   console.log(`Server listening at port ${PORT}`);
